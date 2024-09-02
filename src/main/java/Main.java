db6033991a74574a2ad530c9edb2773f3b469b9f
LukePJ25 <luke.jones.2567@gmail.com>
2024-09-02 17:53:12 +0100

    Commented out stage icon for the time being.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -14,8 +14,8 @@ public class Main extends Application {
         GridPane root = primaryCont.initGetGrid();
 
         Scene mainScene = new Scene(root, 1100, 800);
-        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/resources/icon.png")));
-        primaryStage.getIcons().add(icon);
+        //Image icon = new Image("../../resources/icon.png");
+        //primaryStage.getIcons().add(icon);
         primaryStage.setTitle("ASCIIArtist");
         primaryStage.setScene(mainScene);
         primaryStage.show();
